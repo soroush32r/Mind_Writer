1fce19d756023fee65e0b656f254a9d7614c57c9
Soroush <ranjbar.soroush32@gmail.com>
2023-12-16 15:49:01 +0330

    Home.jsx some style edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,13 @@ const Home = () => {
         <h1 className="mt-15">Welcome to Mind Writer</h1>
         <div className="flex justify-around flex-col md:flex-row w-full">
             <Link to={'/Blog'}>
-              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105">
+              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105 hover:bg-blue-200/80">
                 <h2 className="mb-10">If you want to write a blog</h2>
                 <h3>click here</h3>
               </div>
             </Link>
             <Link to={'/Notebook'}>
-              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105">
+              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105 hover:bg-blue-200/80">
                 <h2 className="mb-10">If you want use a notebook</h2>
                 <h3>click here</h3>
               </div>
