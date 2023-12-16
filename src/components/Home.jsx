5fd5b609f3e763c1a67ef9a05ef6ab040558fb55
Soroush <ranjbar.soroush32@gmail.com>
2023-12-16 15:28:46 +0330

    Make the Home page responsive and change style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,22 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return(
-    <div className="flex justify-center items-center h-screen font-display  bg-gradient-to-br from-blue-50 to-blue-100/100">
-      <div className="flex flex-col w-11/12 h-5/6 items-center justify-around">
+    <div className="flex justify-center items-center h-screen font-display  bg-gradient-to-br from-blue-50 to-blue-100/100 md:">
+      <div className="flex flex-col w-11/12 h-5/6 items-center justify-around text-center">
         <h1 className="mt-15">Welcome to Mind Writer</h1>
-        <div className="flex justify-around w-full">
-          <div className="text-center mb-10">
-            <h2 className="mb-10">If you want to write a blog</h2>
-            <Link to={'/Blog'} className="bg-blue-200 px-2 py-1 rounded text-2xl hover:bg-blue-100">Write a blog</Link>
-          </div>
-          <div className="text-center">
-            <h2 className="mb-10">If you want use a notebook</h2>
-            <Link to={'/Notebook'} className="bg-blue-200 px-2 py-1 rounded text-2xl hover:bg-blue-100">Notebook</Link>
-          </div>
+        <div className="flex justify-around flex-col md:flex-row w-full">
+            <Link to={'/Blog'}>
+              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105">
+                <h2 className="mb-10">If you want to write a blog</h2>
+                <h3>click here</h3>
+              </div>
+            </Link>
+            <Link to={'/Notebook'}>
+              <div className="text-center mb-10 shadow-2xl border rounded-lg bg-blue-200 p-10 hover:scale-105">
+                <h2 className="mb-10">If you want use a notebook</h2>
+                <h3>click here</h3>
+              </div>
+            </Link>
         </div>
 
       </div>
